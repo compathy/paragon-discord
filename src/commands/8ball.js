4723c59fa2585c9903c1cedd426565310f795d16
phineas <phin@phineas.io>
2017-02-10 23:35:39 +0000

    Fix conflict

--- src/commands/8ball.js
+++ src/commands/8ball.js
@@ -3,8 +3,7 @@ var command = {};
 const responses = ["Sure, I guess so.", "Absolutely not.", "Nope, never.", "YES YES YES!", "Of course.", "Why not.", "No way.", "Are you joking? Of course not.", "NOT IN A MILLION YEARS.", "Not sure... ask again.", "Not looking hopeful tbh...", "Definitely.", "This has to be a joke..."];
 
 command.hehe = function(question, callback) {
-  var random = responses[Math.floor(Math.random()*responses.length)];
-
+  let random = responses[Math.floor(Math.random()*responses.length)];
   callback(random);
 }
 
